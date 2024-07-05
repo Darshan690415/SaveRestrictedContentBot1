f7ee1e22300cf1bb64c6313099b5a8d983300cf5
Darshan690415 <darshanmalkiya5@gmail.com>
2024-07-05 21:51:08 +0530

    __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = "25352517"
+API_HASH = "2b7e6cf7752c3af91f958d67793a3e0b"
+BOT_TOKEN = "7396421147:AAGAeJQbnNpmWwEGmIfN4MHj5ARN0e1RXC0"
+SESSION = "BQEtWHgAj_jU8Qg91EdnL56hEhHCYgaueh_j621BwC60jEqFnHgPvhpw3KsOuIw0bLHRL9JK-X4YrZgkJ7cOSWopS9rI1XeKs1R9Sa7Ob3urGMarqOYJUpmqqPWZVLKa_QoE8tIQc70YGairjXYZt51k9Kjkh0PJevHtU0ex00cG3hFCjALaZ42Vacz_xrefFeCMT6b6oqEZRU8FX1Qo0fa4OB3lufQWvaE5CPZWczx6V-C9QPmIxfi9tXaz6mSzO4tbIgK13mcsCZmjen_tdPSw_nHh2GnwF2m6kIzSoceM6K7PyLzyaGfceiYdLtHUW9ad5EnVti6RrtKqz-7Mnk-uOxtL1QAAAAFrTgnxAA"
+FORCESUB = "Bing"
+AUTH = "6095243761"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
